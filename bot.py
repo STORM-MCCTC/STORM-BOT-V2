ac6ef34db0e6f8f29a8b366a181d7f43a1d51b29
STORM-MCCTC <donnadio_mark@student.mahoningctc.com>
2024-09-16 22:07:21 -0400

    Added more Simple commands

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix="~~", intents=discord.Intents.all())
+client = commands.Bot(command_prefix="~", intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
@@ -13,11 +13,27 @@ async def ping(ctx):
 
 @client.command()
 async def h(ctx):
-    await ctx.send("commands: | ~~ping | ~~twitch | ")
+    await ctx.send("commands: | ~ping | ~twitch | ~youtube | ~twitter | ~github | ~website |")
 
 @client.command()
 async def twitch(ctx):
     await ctx.send("https://www.twitch.tv/storm_vtuber")
+
+@client.command()
+async def youtube(ctx):
+    await ctx.send("https://www.youtube.com/@STORM_VTUBER")
+
+@client.command()
+async def twitter(ctx):
+    await ctx.send("https://x.com/STORM_Plus")
+
+@client.command()
+async def github(ctx):
+    await ctx.send("https://github.com/STORM-MCCTC")
+
+@client.command()
+async def website(ctx):
+    await ctx.send("https://storm-mcctc.github.io/STORM-Funny-webpage/")
 
 with open("Bot_token.txt") as f:
     token = f.read()
